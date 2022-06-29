7e452038b1b7606915eeebcf97c0df9b3ea8cc5c
Leejinsil <hby04011@naver.com>
2022-06-29 14:21:24 +0900

    java

---
+++ JAVA example/MakeStar/CountingStar.java
@@ -0,0 +1,32 @@
+public class CountingStar {
+
+    public static void main(String[] arg) {
+
+    //직사각형 만들기 
+/*      int i;
+        int j;
+
+        for(i=0; i<6; i++) {
+            for(j=0;j<6; j++) {
+                System.out.print("*");
+            }System.out.println("*");
+        }
+    } */
+    
+
+    //직각삼각형 만들기
+        int i;
+        int j;
+
+        for(i=0; i<6; i++) {
+            for(j=0; j<6; j++) {
+                System.out.println("*");
+            }System.out.println("*");
+        }
+    
+    //정삼각형 만들기
+    //마름모 만들기
+    //원 만들기 
+
+
+    }
---
+++ JAVA example/exam_typechange/Child.java
@@ -0,0 +1,17 @@
+public class Child {
+    public class Child extends Parent {
+
+        public Child(String input_bagName, int input_money) {
+            super(input_bagName, input_money);
+        }
+    }
+
+    @Override
+    public void goToWork() {
+        System.out.println("나는 학교에 출근합니다");
+    }
+
+    public void goToPCRoom() {
+        System.out.println("나는 PC방에 갑니다");
+    }
+}
