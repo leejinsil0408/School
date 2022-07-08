456c2be315a4303589b213d4bf0b99000a766a67
Leejinsil <hby04011@naver.com>
2022-07-08 21:22:48 +0900

    example

---
+++ JAVA/example/example_super/SupersonicAirplaneExample.java
@@ -0,0 +1,14 @@
+package exam_super;
+
+public class SupersonicAirplaneExample {
+    public static void main(String[] args) {
+        SupersonicAirplane sa = new SupersonicAirplane();
+        sa.takeOff();
+        sa.fly();
+        sa.flymode = SupersonicAirplane.SUPERSONIC;
+        sa.fly();
+        sa.flymode = SupersonicAirplane.NORMAL;
+        sa.fly();
+        sa.land();
+    }
+}
