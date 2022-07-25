0a54a34c20bd6533fd5aec0569265476938d8f28
leejinsil0408 <hby04011@naver.com>
2022-07-25 21:30:40 +0900

    example

--- JAVA/Example/example_Nested/Object/A.java
+++ JAVA/Example/example_Nested/Object/A.java
@@ -4,39 +4,45 @@ package example_Nested.Object;
 public class A {
     A() {
         System.out.println("A객체가 생성됨");
-    }
-    /*인스턴스 멤버 클래스*/
-    class B {
-        B () {
-            System.out.println("B객체가 생성됨");
-        }
-        int field1;
-        static int field2;
-        void mathod1() {};
-        static void mathod2() {}
-    }
-    /*정적 멤버 클래스*/
-    static class C {
-        C() {System.out.println("C객체가 생성됨");}
-        int field1;
-        static int field2;
-        void method1() {};
-        static void method2() {};
+
+        B b = new B();
+        b.field1 = 3;
+        b.method1();
     }
 
+    class B {
+        int field1;
+        B() {
+            System.out.println("B객체가 생성됨");
+        }
+        void method1() {};
+    }
+
+    static class C {
+        int field1;
+        static int field2;
+        C() {
+            System.out.println("C 객체가 생성됨");
+        }
+        void method1() {}
+        static void method2() {}
+    }
     void method() {
-        /*로컬 클래스*/
         class D {
-            D() {
-                System.out.println("D 객체가 생성됨");
-            }
             int field1;
-            static int field2;
-            void method1() {};
-            static void method2() {};
+            D () {
+                System.out.println("D객체가 생성됨");
+            }
+            void method1() {}
         }
         D d = new D();
         d.field1 = 3;
         d.method1();
     }
 }
+
+
+
+
+
+
--- JAVA/Example/example_Nested/Object/Main.java
+++ JAVA/Example/example_Nested/Object/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //인스턴스 멤버 클래스 객체 생성
         A.B b = a.new B();
         b.field1 = 3;
-        b.mathod1();
+        b.method1();
 
         //정적 멤버 클래스 객체 생성
         A.C c = new A.C();
