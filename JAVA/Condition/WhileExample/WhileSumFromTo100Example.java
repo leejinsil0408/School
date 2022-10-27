7117f238d7aecf036009a125c0465a0daafa6135
leejinsil0408 <hby04011@naver.com>
2022-10-27 17:31:49 +0900

    20221027

---
+++ JAVA/Condition/WhileExample/WhileSumFromTo100Example.java
@@ -0,0 +1,14 @@
+package Condition.WhileExample;
+
+public class WhileSumFromTo100Example {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1;
+
+        while (i<=1) {
+            sum += i;
+            i++;
+        }
+        System.out.println("1~" + i + " 합 : " + sum);
+    }
+}
